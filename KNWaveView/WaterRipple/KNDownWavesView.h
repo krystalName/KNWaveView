124f49a81bee67c48b728f9b7b54d05437af6b5c
Leessang <992275294@qq.com>
2017-10-26 15:23:28 +0800

    up load

--- KNWaveView/WaterRipple/KNDownWavesView.h
+++ KNWaveView/WaterRipple/KNDownWavesView.h
@@ -10,4 +10,25 @@
 
 @interface KNDownWavesView : UIView
 
+
+/**
+ 水波纹的高度
+ */
+@property(nonatomic, assign)CGFloat WaveY; //默认在创建view高度／2
+
+/**
+ 底部水波纹的颜色
+ */
+@property(nonatomic, strong)UIColor  *bottomColor; //默认为黄色 0.4透明度
+
+/**
+ 顶部水波纹的颜色
+ */
+@property(nonatomic, strong)UIColor *topColor;  //默认黄色 0.8 透明度
+
+/**
+ 水波纹振动幅度
+ */
+@property(nonatomic, assign)CGFloat waveAmplitude;  //默认20。
+
 @end
